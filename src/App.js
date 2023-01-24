d0d76ee89f20881b07a5163b25d448d90e96a62f
edenlisk <nkumbuyenock@gmail.com>
2023-01-24 17:25:09 +0200

    Add routes to different website locations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 /* eslint-disable react/prefer-stateless-function */
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import Navbar from './components/navigation/Navbar';
 import Calculator from './components/calculator';
+import Home from './components/pages/Home/Home';
+import Quote from './components/pages/Quote/Quote';
 
 class App extends React.Component {
   render() {
     return (
-      <Calculator />
+      <>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/quote" element={<Quote />} />
+          </Routes>
+        </Router>
+      </>
     );
   }
 }
